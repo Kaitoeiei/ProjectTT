55531ed66c7f8a6768559a6559df996e0f2b3852
Kaitoeiei <640107030038@dpu.ac.th>
2023-11-28 02:30:06 +0700

    connect Mysql

--- kaito/backend/index.js
+++ kaito/backend/index.js
@@ -1,14 +1,28 @@
 const express = require('express');
 const app = express();
-const mysql = require('mysql');
-const cors = require('cors');
+const mysql2 = require('mysql2');
+const cors = require('cors')
 
 app.use(cors());
 app.use(express.json());
 
-const db = mysql.createConnection({
+const db = mysql2.createConnection({
     user:"root",
     host:"localhost",
     password:"sirisan123",
     database:"customer"
 })
+
+app.get('/customer', (req,res) => {
+    db.query("SELECT * FROM customer" , (err,result) =>{
+        if(err){
+            console.log(err)
+        }else{
+            res.send(result);
+        }
+    });
+});
+
+app.listen('3001', () =>{
+    console.log('Server is running on port 3001');
+});
